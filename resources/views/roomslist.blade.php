eb6f2e38c5e29fa374961b2523c3582da1621688
Zafar <naveedulz@gmail.com>
2022-03-21 18:56:22 +0100

    added route web single room

--- resources/views/roomslist.blade.php
+++ resources/views/roomslist.blade.php
@@ -27,6 +27,7 @@
             <div class="col-lg-9 col-12">
                 @foreach ($roomListAll as $item)
                 <!-- ITEM -->
+                
                 <div class="room-list-item">
                     <div class="row">
                         <div class="col-lg-5">
